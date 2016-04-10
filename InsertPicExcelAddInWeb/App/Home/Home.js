52eb6824d7c667030ac14c17145ebdf927cce155
Thomas Mathew <tmathew1000@hotmail.com>
2016-04-10 16:48:20 -0500

    Initial Submision

---
+++ InsertPicExcelAddInWeb/App/Home/Home.js
@@ -0,0 +1,51 @@
+﻿/// <reference path="../App.js" />
+
+(function () {
+    "use strict";
+
+    // The initialize function must be run each time a new page is loaded
+    Office.initialize = function (reason) {
+        $(document).ready(function () {
+            app.initialize();
+
+            $('#insertimage').click(toDataUrl);
+        });
+    };
+
+    function insertPictureAtSelection(base64EncodedImageStr) {
+
+        Office.context.document.setSelectedDataAsync(base64EncodedImageStr, {
+            coercionType: Office.CoercionType.Image,
+            imageLeft: 50,
+            imageTop: 50,
+            imageWidth: 200,
+            imageHeight: 200
+        },
+           function (asyncResult) {
+               if (asyncResult.status === Office.AsyncResultStatus.Failed) {
+                   console.log("Action failed with error: " + asyncResult.error.message);
+               }
+           });
+    }
+
+    function toDataUrl() {
+        var xhr = new XMLHttpRequest();
+        var url = "https://localhost:44300/api/test/GetImage"
+        xhr.open('GET', url, true);
+
+        //xhr.responseType = 'blob';
+
+        xhr.onload = function (e) {
+            if (this.status == 200) {
+                insertPictureAtSelection(this.response);
+            }
+        };
+
+        xhr.onerror = function (e) {
+            alert("Error " + e.target.status + " occurred while receiving the document.");
+        };
+
+        xhr.send();
+    }
+    
+})();
---
+++ TestWebAPI/Controllers/TestController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using System.Drawing;
+
+
+namespace TestCORS.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class TestController : ApiController
+    {
+        public string GetImage()
+        {
+            System.Drawing.Image image = DownloadImageFromUrl("http://www.whitegadget.com/attachments/pc-wallpapers/85254d1320380902-house-house-wallpaper.jpg");
+            string imgstr= ImageToBase64(image, System.Drawing.Imaging.ImageFormat.Png);
+            return imgstr;
+        }
+
+        private System.Drawing.Image DownloadImageFromUrl(string imageUrl)
+        {
+            System.Drawing.Image image = null;
+
+            try
+            {
+                System.Net.HttpWebRequest webRequest = (System.Net.HttpWebRequest)System.Net.HttpWebRequest.Create(imageUrl);
+                webRequest.AllowWriteStreamBuffering = true;
+                webRequest.Timeout = 30000;
+
+                System.Net.WebResponse webResponse = webRequest.GetResponse();
+
+                System.IO.Stream stream = webResponse.GetResponseStream();
+
+                image = System.Drawing.Image.FromStream(stream);
+
+                webResponse.Close();
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+
+            return image;
+        }
+
+        private string ImageToBase64(Image image,  System.Drawing.Imaging.ImageFormat format)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                // Convert Image to byte[]
+                image.Save(ms, format);
+                byte[] imageBytes = ms.ToArray();
+
+                // Convert byte[] to Base64 String
+                string base64String = Convert.ToBase64String(imageBytes);
+                return base64String;
+            }
+        }
+    }
+}
